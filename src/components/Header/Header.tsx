e9138310cac772f22b84210fc9113ddf49c80c20
ananthutk <ananthutk97@gmail.com>
2020-09-20 14:26:07 +0530

    Heading style change

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 function Header() {
   return (
     <div>
-      <h1>Covid 19 Tracker India</h1>
+      <h1 style={{fontWeight:100}} >Covid 19 Tracker India</h1>
     </div>
   )
 }
