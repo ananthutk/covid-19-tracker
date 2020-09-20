daa6b915cbf57d3999d2ed574893f40466bfd276
ananthutk <ananthutk97@gmail.com>
2020-09-20 14:09:51 +0530

    Updates

---
+++ precache-manifest.ba67f58b9bf9ed5cbbfbf5c2b37bbc92.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "418e0437dbe57a4e223460fd5eab19c6",
+    "url": "/covid-19-tracker/index.html"
+  },
+  {
+    "revision": "e2e0236e22444e8e79d8",
+    "url": "/covid-19-tracker/static/css/main.51de446f.chunk.css"
+  },
+  {
+    "revision": "0666dba78249e35252f9",
+    "url": "/covid-19-tracker/static/js/2.47b6d2a3.chunk.js"
+  },
+  {
+    "revision": "ccb96163cb81453816364e1c2beb6525",
+    "url": "/covid-19-tracker/static/js/2.47b6d2a3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e2e0236e22444e8e79d8",
+    "url": "/covid-19-tracker/static/js/main.834eab0e.chunk.js"
+  },
+  {
+    "revision": "fb403f6422f9b7442ea3",
+    "url": "/covid-19-tracker/static/js/runtime-main.960a8408.js"
+  }
+]);
